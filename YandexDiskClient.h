e6862ebb6714ff112ad356aa39e0f240418b1a1f
Krasnovvvvv <164088576+Krasnovvvvv@users.noreply.github.com>
2025-07-11 18:03:18 +0300

    added signatures of upload and download functions

--- YandexDiskClient.h
+++ YandexDiskClient.h
@@ -14,10 +14,16 @@ public:
     bool publish(const std::string& path);
     std::string getPublicDownloadLink(const std::string& path);
     std::string formatResourceList(const nlohmann::json& json);
+    bool uploadFile(const std::string& upload_disk_path, const std::string& local_path);
+    bool downloadFile(const std::string& download_disk_path, const std::string& local_path);
+
 
 private:
     std::string token;
     std::string performRequest(const std::string& url, const std::string& method = "GET");
+    std::string getUploadUrl(const std::string& upload_disk_path);
+    std::string getDownloadUrl(const std::string& download_disk_path);
+
 
 };
 
