177e1f8d08d81659ed581698aac7e8a4444de36e
Krasnovvvvv <164088576+Krasnovvvvv@users.noreply.github.com>
2025-07-12 23:45:16 +0300

    the function of uploading a file to disk uploadFile() has been implemented

--- YandexDiskClient.cpp
+++ YandexDiskClient.cpp
@@ -10,7 +10,48 @@ size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
 YandexDiskClient::YandexDiskClient(const std::string& oauth_token)
         : token(oauth_token) {}
 
-std::string YandexDiskClient::performRequest(const std::string& url, const std::string& method) {
+std::string YandexDiskClient::buildUrl(
+        const std::string& endpoint,
+        const std::string& path,
+        const std::string& extraParams
+) {
+    CURL* curl = curl_easy_init();
+    if (!curl) throw std::runtime_error("curl_easy_init() failed");
+
+    char* escaped = curl_easy_escape(curl, path.c_str(), 0);
+    if (!escaped) {
+        curl_easy_cleanup(curl);
+        throw std::runtime_error("curl_easy_escape() failed");
+    }
+
+    std::string url = endpoint + escaped + extraParams;
+    curl_free(escaped);
+    curl_easy_cleanup(curl);
+
+    return url;
+}
+
+std::string YandexDiskClient::getLinkByKey(
+        const std::string& path,
+        const std::string& endpoint,
+        const std::string& key,
+        const std::string& extraParams,
+        const std::string& errorMsg
+) {
+    std::string url = buildUrl(endpoint, path, extraParams);
+    std::string resp = performRequest(url);
+    auto json = nlohmann::json::parse(resp);
+
+    if (json.contains(key) && !json[key].is_null())
+        return json[key].get<std::string>();
+    else if (json.contains("error"))
+        throw std::runtime_error("Yandex.Disk API error: " + json["error"].get<std::string>());
+    else
+        throw std::runtime_error(errorMsg);
+}
+
+std::string YandexDiskClient::performRequest(const std::string& url,
+                                             const std::string& method) {
     CURL* curl = curl_easy_init();
     std::string response;
     if (!curl) throw std::runtime_error("curl_easy_init() failed");
@@ -42,6 +83,22 @@ nlohmann::json YandexDiskClient::getResourceList(const std::string& path) {
     return nlohmann::json::parse(resp);
 }
 
+std::string YandexDiskClient::formatResourceList(const nlohmann::json& json) {
+    std::ostringstream oss;
+    int idx = 1;
+    for (const auto& item : json["_embedded"]["items"]) {
+        oss << idx++ << ". " << item["name"].get<std::string>() << "\n";
+        oss << "   Type: " << item["type"].get<std::string>() << "\n";
+        oss << "   Path: " << item["path"].get<std::string>() << "\n";
+        if (item.contains("public_url"))
+            oss << "   Public URL: " << item["public_url"].get<std::string>() << "\n";
+        else
+            oss << "   Public URL: is missing\n";
+        oss << "\n";
+    }
+    return oss.str();
+}
+
 bool YandexDiskClient::publish(const std::string& path) {
     std::string url = buildUrl(
             "https://cloud-api.yandex.net/v1/disk/resources/publish?path=",
@@ -62,26 +119,6 @@ bool YandexDiskClient::publish(const std::string& path) {
     return true;
 }
 
-
-std::string YandexDiskClient::getLinkByKey(
-        const std::string& path,
-        const std::string& endpoint,
-        const std::string& key,
-        const std::string& extraParams,
-        const std::string& errorMsg
-) {
-    std::string url = buildUrl(endpoint, path, extraParams);
-    std::string resp = performRequest(url);
-    auto json = nlohmann::json::parse(resp);
-
-    if (json.contains(key) && !json[key].is_null())
-        return json[key].get<std::string>();
-    else if (json.contains("error"))
-        throw std::runtime_error("Yandex.Disk API error: " + json["error"].get<std::string>());
-    else
-        throw std::runtime_error(errorMsg);
-}
-
 std::string YandexDiskClient::getPublicDownloadLink(const std::string& path) {
     return getLinkByKey(
             path,
@@ -91,22 +128,6 @@ std::string YandexDiskClient::getPublicDownloadLink(const std::string& path) {
             "The file or directory has not been published! "
             "Use publish() method to publish it."
     );
-}
-
-std::string YandexDiskClient::formatResourceList(const nlohmann::json& json) {
-    std::ostringstream oss;
-    int idx = 1;
-    for (const auto& item : json["_embedded"]["items"]) {
-        oss << idx++ << ". " << item["name"].get<std::string>() << "\n";
-        oss << "   Type: " << item["type"].get<std::string>() << "\n";
-        oss << "   Path: " << item["path"].get<std::string>() << "\n";
-        if (item.contains("public_url"))
-            oss << "   Public URL: " << item["public_url"].get<std::string>() << "\n";
-        else
-            oss << "   Public URL: is missing\n";
-        oss << "\n";
-    }
-    return oss.str();
 }
 
 std::string YandexDiskClient::getUploadUrl(const std::string& upload_disk_path) {
@@ -129,26 +150,43 @@ std::string YandexDiskClient::getDownloadUrl(const std::string& download_disk_pa
     );
 }
 
-std::string YandexDiskClient::buildUrl(
-        const std::string& endpoint,
-        const std::string& path,
-        const std::string& extraParams
-) {
-    CURL* curl = curl_easy_init();
-    if (!curl) throw std::runtime_error("curl_easy_init() failed");
+bool YandexDiskClient::uploadFile(const std::string& upload_disk_path,
+                                  const std::string& local_path) {
+    std::string url = getUploadUrl(upload_disk_path);
 
-    char* escaped = curl_easy_escape(curl, path.c_str(), 0);
-    if (!escaped) {
-        curl_easy_cleanup(curl);
-        throw std::runtime_error("curl_easy_escape() failed");
+    FILE* file = fopen(local_path.c_str(), "rb");
+    if (!file) {
+        throw std::runtime_error("Couldn't open the file: " + local_path);
     }
 
-    std::string url = endpoint + escaped + extraParams;
-    curl_free(escaped);
+    CURL* curl = curl_easy_init();
+    if (!curl) {
+        fclose(file);
+        throw std::runtime_error("curl_easy_init() failed");
+    }
+
+    fseek(file, 0, SEEK_END);
+    long filesize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+    curl_easy_setopt(curl, CURLOPT_PUT, 1L);
+    curl_easy_setopt(curl, CURLOPT_READDATA, file);
+    curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)filesize);
+
+    CURLcode res = curl_easy_perform(curl);
+
+    fclose(file);
     curl_easy_cleanup(curl);
 
-    return url;
+    if (res != CURLE_OK) {
+        throw std::runtime_error("File upload error: " + std::string(curl_easy_strerror(res)));
+    }
+
+    return true;
 }
+
 
 
 
--- YandexDiskClient.h
+++ YandexDiskClient.h
@@ -8,34 +8,34 @@
 
 class YandexDiskClient {
 public:
-    explicit YandexDiskClient(const std::string& oauth_token);
+    explicit YandexDiskClient(const std::string& );
 
     nlohmann::json getResourceList(const std::string& path ="/");
-    bool publish(const std::string& path);
-    std::string getPublicDownloadLink(const std::string& path);
-    std::string formatResourceList(const nlohmann::json& json);
-    bool uploadFile(const std::string& upload_disk_path, const std::string& local_path);
+    std::string formatResourceList(const nlohmann::json& );
+    bool publish(const std::string& );
+    std::string getPublicDownloadLink(const std::string& );
+    bool uploadFile(const std::string& , const std::string& );
     bool downloadFile(const std::string& download_disk_path, const std::string& local_path);
 
 
 private:
     std::string token;
-    std::string performRequest(const std::string& url, const std::string& method = "GET");
-    std::string getUploadUrl(const std::string& upload_disk_path);
-    std::string getDownloadUrl(const std::string& download_disk_path);
+    std::string performRequest(const std::string& , const std::string& method = "GET");
+    std::string getUploadUrl(const std::string& );
+    std::string getDownloadUrl(const std::string& );
 
     std::string getLinkByKey(
-            const std::string& path,
-            const std::string& endpoint,
-            const std::string& key,
-            const std::string& extraParams,
-            const std::string& errorMsg
+            const std::string& ,
+            const std::string& ,
+            const std::string& ,
+            const std::string& ,
+            const std::string&
     );
 
     std::string buildUrl(
-            const std::string& endpoint,
-            const std::string& path,
-            const std::string& extraParams
+            const std::string& ,
+            const std::string& ,
+            const std::string&
     );
 
 
